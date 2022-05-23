5f71add3356ebc9eb6813837746d56bfd313c142
Yevhen Zavelya <koshanua@gmail.com>
2022-05-23 16:00:20 +0300

    "Little commit 4"

--- Lesson3.java
+++ Lesson3.java
@@ -1,7 +1,8 @@
 public class Lesson3 {
     public static void main(String[] args)
             throws java.io.IOException {
-
+                Shrek.main();
+            /*
             char ch;
 
             System.out.print("Press the button and then press \"ENTER\": ");
@@ -9,5 +10,24 @@ public class Lesson3 {
             ch = (char) System.in.read();
 
             System.out.println("You pressed the button " + ch);
+
+
+            */
+    }
+}
+
+
+class Shrek {
+    public static void main()
+        throws java.io.IOException {
+        char ch, answer = 'k';
+        System.out.println("Try your luck, enter the character A-Z: ");
+        ch = (char) System.in.read();
+        if (ch == answer) {
+            System.out.println("You are right! The letter is " + answer);
+        }
+        else {
+            System.out.println("Symbol is incorrect, try again.");
+        }
     }
 }
